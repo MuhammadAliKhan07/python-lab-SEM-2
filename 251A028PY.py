b4eaaced20aa3e119cb3af245d1f8dfb98bc1e94
MuhammadAliKhan07 <muhammadalik730@gmail.com>
2026-01-20 09:59:46 +0530

    Add files via upload

---
+++ 251A028PY.py
@@ -0,0 +1,13 @@
+radius = float(input("enter the radius: "))
+
+print(f"your radius is {radius}")
+area = 3.142 * radius * radius
+print(f"area of circle is {area}")
+lengh = input("enter the lengh: ")
+width = input("enter the width: ")
+area = int(lengh) * int(width)
+print(f"area of rectangle is {area}")
+side = input("enter the side: ")
+area = int(side) * int(side)
+print(f"area of square is {area}")
+
