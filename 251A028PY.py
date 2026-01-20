071568548b924249b6bb218bfea66781c234638c
MuhammadAliKhan07 <muhammadalik730@gmail.com>
2026-01-20 10:14:35 +0530

    Refactor area calculations with section headers

--- 251A028PY.py
+++ 251A028PY.py
@@ -1,13 +1,16 @@
+print("Calculation of Area of Circle")
 radius = float(input("enter the radius: "))
-
 print(f"your radius is {radius}")
 area = 3.142 * radius * radius
 print(f"area of circle is {area}")
+print("Calculation of Area of Rectangle")
 lengh = input("enter the lengh: ")
 width = input("enter the width: ")
 area = int(lengh) * int(width)
 print(f"area of rectangle is {area}")
+print("Calculation of Area of Square")
 side = input("enter the side: ")
 area = int(side) * int(side)
 print(f"area of square is {area}")
+
 
