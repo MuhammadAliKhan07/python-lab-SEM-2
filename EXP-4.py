e566114cadec3bae75be75699eff32865218a3c4
MuhammadAliKhan07 <muhammadalik730@gmail.com>
2026-01-27 09:46:32 +0530

    Add files via upload

---
+++ EXP-4.py
@@ -0,0 +1,8 @@
+
+import math
+p = float(input("Enter your Principle Amount(in Rupees): Rs"))
+roi = float(input("Enter the Rate Of Interest(ROI): "))
+time_period = float(input("Enter the time period(In Years): ")) 
+simple_interest = (p * roi * time_period) / 100
+print(f"Simple Interest: {simple_interest}Rs")
+print("UIN: 251A028, Date: 27/01/2026")
